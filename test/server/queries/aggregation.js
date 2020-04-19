21c489fe6cf9c09cfd284132fc5b2fd8f12548c7
charlie.chen <charlie.chen.v@gmail.com>
2020-04-19 15:18:02 -0600

    fix test

--- test/server/queries/aggregation.js
+++ test/server/queries/aggregation.js
@@ -24,17 +24,23 @@ const queries = [
     sql: `
       query {
         queryAuthorJoinPost(
-          where: { post_id: { gt: 2 }, id: {gt: 3} }
-          orderBy: [{ column: "score__sum", order: "desc" }]
+          where: { post_id: { gt: 2 }, id: { gt: 3 } }
+          orderBy: [{ column: "score__max", order: "desc" }]
           limit: 10
           offset: 0
           groupBy: ["id", "email"]
-          having: { age__avg: { gt: 30 }, id: { nin: [1, 2, 6] }, email: {null: false} }
+          having: {
+            age__avg: { gt: 30 }
+            id: { nin: [1, 2, 6] }
+            email: { null: false }
+          }
         ) {
           id
           email
           age__avg
-          score__sum
+          age__count
+          age__count_distinct
+          score__max
           review {
             id
             star
